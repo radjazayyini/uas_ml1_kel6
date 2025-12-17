46df45ea66ec351868b74bbadf1bb6fadb8ba15c
radjazayyini <141828599+radjazayyini@users.noreply.github.com>
2025-12-17 20:33:56 +0700

    Update appv2.py

--- appv2.py
+++ appv2.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import pickle
 from sklearn.metrics import mean_absolute_error, mean_squared_error
